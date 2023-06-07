f0ed1423391665f03597af65df751bb2b8843e8f
james <arvinjamesinanoria@gmail.com>
2023-06-06 22:25:16 -0700

    fixed and added stuff to design section elements

--- server.js
+++ server.js
@@ -13,4 +13,16 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log("Listening on http://localhost:" + PORT);
-});
+}); 
+
+/* const express = require('express');
+const { ApolloServer } = require ('apollo-server-express');
+
+const app = express();
+const PORT =process.env.PORT || 3001;
+const server = new ApolloServer({
+
+})
+
+const startApolloServer = async () */
+
